69929b743bfe7c7f3dd91887d2658979efd63fe4
MrSteveW <mrstevenwilliams@gmail.com>
2025-09-19 07:41:40 +0100

    Add warning display

--- app.js
+++ app.js
@@ -1,61 +1,53 @@
 // https://cookie-upgrade-api.vercel.app/api/upgrades
 const brain = document.querySelector("img");
-const cookiesDisplay = document.getElementById("numberCookies");
-const cpsDisplay = document.getElementById("cps");
+const neuronsDisplay = document.getElementById("numberCookies");
+const npsDisplay = document.getElementById("nps");
 const upgradeDisplay = document.getElementById("upgrade-display");
-const upgrade1 = document.getElementById("upgrade1");
-let exampleData = [
-  {
-    id: 1,
-    name: "Auto-Clicker",
-    cost: 100,
-    increase: 1,
-  },
-  {
-    id: 2,
-    name: "Enhanced Oven",
-    cost: 500,
-    increase: 5,
-  },
-];
+const warningDisplay = document.getElementById("warning");
 
-// JS vars cookieCount, cps, state
-let cookieCount = 0;
-let cps = 1;
+// JS vars neuronCount, nps, state
+let neuronCount = 0;
+let nps = 1;
 let state = {
-  cookieCount: 0,
-  cps: 1,
+  neuronCount: 0,
+  nps: 1,
 };
 
-//start
+//startup
+loadUpgrades();
 
 brain.addEventListener("click", () => {
-  cookieCount += cps;
-  cookiesDisplay.textContent = cookieCount;
+  neuronCount += nps;
+  neuronsDisplay.textContent = neuronCount;
+});
+
+function setWarning() {
+  warningDisplay.style.display = "inline";
+  setTimeout(() => {
+    warningDisplay.style.display = "none";
+  }, 2000);
+}
+
+neuronsDisplay.addEventListener("click", () => {
+  setWarning();
 });
 
 // upgrade1.addEventListener("click", () => {
-//   cps++;
-//   cpsDisplay.textContent = cps;
+//   nps++;
+//   npsDisplay.textContent = nps;
 // });
 
 setInterval(() => {
-  cookieCount += cps;
-  cookiesDisplay.textContent = cookieCount;
+  neuronCount += nps;
+  neuronsDisplay.textContent = neuronCount;
 }, 1000);
 
-// async function fetchData() {
-//   const response = await fetch(
-//     "https://cookie-upgrade-api.vercel.app/api/upgrades"
-//   );
-//   const data = await response.json();
-
-// }
-
-// fetchData();
-
-function displayUpgrades() {
-  exampleData.forEach(function (upgrade, index) {
+async function loadUpgrades() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  const data = await response.json();
+  data.forEach(function (upgrade, index) {
     const upgradeName = document.createElement("div");
     upgradeName.innerText = upgrade.name;
     upgradeName.setAttribute("class", "upg");
@@ -71,9 +63,12 @@ function displayUpgrades() {
     const upgradeStock = document.createElement("div");
     upgradeStock.innerText = 0;
     upgradeStock.setAttribute("class", "upg");
+    //
     const upgradeButton = document.createElement("button");
     upgradeButton.innerText = "Buy";
-    upgradeStock.setAttribute("class", "upg");
+    // upgradeButton.setAttribute("id", upgrade.id);
+    upgradeButton.id = "upgbutton" + upgrade.id;
+    upgradeButton.setAttribute("class", "upg");
     //
     upgradeDisplay.append(
       upgradeName,
@@ -82,21 +77,24 @@ function displayUpgrades() {
       upgradeStock,
       upgradeButton
     );
+    upg1 = document.getElementById("upgbutton1");
+    upg1.addEventListener("click", () => {
+      nps += 1;
+      npsDisplay.textContent = nps;
+    });
   });
 }
-
-displayUpgrades();
 
 // the upgrades are an array of objects
 
 // we can use forEach or a for loop to loop over each of the upgrades.
-// use create element to create tags for the upgrade name, cps, and increase.
+// use create element to create tags for the upgrade name, nps, and increase.
 // also need to make a buy button.
 // listen for a click event on each of the buttons.
 // try to purchase the upgrade
 // can we afford the upgrade?
 // if we can afford the upgrade -
-// update cps to add the increase of the upgrade
+// update nps to add the increase of the upgrade
 // take away the cost of the upgrade from our cookies
 // if we can't afford the upgrade?
 // send an alert to say 'you can't afford that.
